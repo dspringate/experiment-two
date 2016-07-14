fa3ec19a40e2ee44b63d2feab243c5585f79b15b
dspringate <info@hatchinc.co>
2016-07-14 10:23:05 +0100

    changes

---
+++ _site/js/main.js
@@ -0,0 +1,33 @@
+// Site functions
+window.site = window.site || {};
+
+site.cacheSelectors = function () {
+	site.cache = {
+		// General
+		$html         : $('html'),
+		$body         : $('document.body')
+	};
+};
+
+site.init = function () {
+	FastClick.attach(document.body);
+	site.cacheSelectors();
+	site.smoothScroll();
+};
+
+site.smoothScroll = function () {
+	$(document).ready(function() {
+    	$(".learn-more").click( function(event) {
+    		event.preventDefault();
+    		//calculate destination place
+    		var dest = 0;
+    		if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+    			dest = $(document).height() - $(window).height();
+    		} else {
+    			dest = $(this.hash).offset().top;
+    		}
+    		// go to destination
+    		$('html, body').animate({scrollTop:dest}, 1000, 'swing');
+    	});
+ 	});
+};
