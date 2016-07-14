0833296f661aa914d15de7372f92d9099109043b
dspringate <info@hatchinc.co>
2016-07-14 10:28:12 +0100

    changes to site

--- _site/js/main.js
+++ _site/js/main.js
@@ -16,18 +16,19 @@ site.init = function () {
 };
 
 site.smoothScroll = function () {
-	$(document).ready(function() {
-    	$(".scroll").click( function(event) {
-    		event.preventDefault();
-    		//calculate destination place
-    		var dest = 0;
-    		if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-    			dest = $(document).height() - $(window).height();
-    		} else {
-    			dest = $(this.hash).offset().top;
-    		}
-    		// go to destination
-    		$('html, body').animate({scrollTop:dest}, 1000, 'swing');
-    	});
- 	});
+	$(".scroll").click( function(event) {
+		event.preventDefault();
+		//calculate destination place
+		var dest = 0;
+		if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+			dest = $(document).height() - $(window).height();
+		} else {
+			dest = $(this.hash).offset().top;
+		}
+		// go to destination
+		$('html, body').animate({scrollTop:dest}, 1000, 'swing');
+	});
 };
+
+// Initialise Site's JS on docready
+$(site.init);
