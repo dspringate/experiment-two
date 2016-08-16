a3a39d57fc5e5a217ce3872bb6ffdac751b0c633
dspringate <info@hatchinc.co>
2016-08-16 13:50:29 +0100

    changes

--- _site/js/main.js
+++ _site/js/main.js
@@ -18,6 +18,7 @@ site.cacheSelectors = function () {
 site.init = function () {
 	site.cacheSelectors();
 	site.smoothScroll();
+	site.expandSearch();
 };
 
 site.smoothScroll = function () {
@@ -33,6 +34,48 @@ site.smoothScroll = function () {
 		// go to destination
 		$('html, body').animate({scrollTop:dest}, 1000, 'swing');
 	});
+};
+
+site.expandSearch = function () {
+	$(document).ready(function(){
+        var submitIcon = $('.searchbox-icon');
+        var inputBox = $('.searchbox-input');
+        var searchBox = $('.searchbox');
+        var isOpen = false;
+        submitIcon.click(function(){
+            if(isOpen == false){
+                searchBox.addClass('searchbox-open');
+                inputBox.focus();
+                isOpen = true;
+            } else {
+                searchBox.removeClass('searchbox-open');
+                inputBox.focusout();
+                isOpen = false;
+            }
+        });  
+         submitIcon.mouseup(function(){
+                return false;
+            });
+        searchBox.mouseup(function(){
+                return false;
+            });
+        $(document).mouseup(function(){
+                if(isOpen == true){
+                    $('.searchbox-icon').css('display','block');
+                    submitIcon.click();
+                }
+            });
+    });
+    function buttonUp(){
+        var inputVal = $('.searchbox-input').val();
+        inputVal = $.trim(inputVal).length;
+        if( inputVal !== 0){
+            $('.searchbox-icon').css('display','none');
+        } else {
+            $('.searchbox-input').val('');
+            $('.searchbox-icon').css('display','block');
+        }
+    }
 };
 
 // Initialise Site's JS on docready
